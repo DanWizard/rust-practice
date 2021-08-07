2d5339ff21a4f64007cf0e3901ae580b6f56b134
DanWizard <tortortorented@gmail.com>
2021-08-07 13:03:34 -0500

    new apps

--- parsing-json/src/main.rs
+++ parsing-json/src/main.rs
@@ -1,3 +1,33 @@
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
+
+use serde_json::Value as JsonValue;
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    is_male: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let json_str = r#"
+        {
+            "name" : "Daniel",
+            "age" : 22,
+            "is_male" : true
+        }
+    "#;
+
+    let res = serde_json::from_str(json_str);
+
+    if res.is_ok() {
+        // let p: JsonValue = res.unwrap();
+        let p: Person = res.unwrap();
+        println!("name {}", p.name)
+    } else {
+        println!("sorry couldd not pass")
+    }
 }
